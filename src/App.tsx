d6b0de7e66167a250032f6bc12c6d965189586d1
Petrik Diak <diak@petrik.hu>
2024-03-12 10:31:41 +0100

    table generate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { Alkotás } from './alkotás'
 import './App.css'
 
 function App() {
  
+  const [alkotáss,setAlkotáss]=useState([] as Alkotás[])
+  useEffect(()=>{
+    async function load() {
+      try
+      {
+        const response=await fetch("http://localhost:3000/commissions");
+        const data= await response.json() as Alkotás[]
+        setAlkotáss(data)
+        console.log(data)
+      }
+      catch{
+
+      }
+    }
+    load()
+  }, [])
 
   return (
     <>
-      
+      <table>
+        {alkotáss.map((x)=>(
+          <tr>
+            <td>{x.id}</td>
+            <td>{x.leiras}</td>
+            <td>{x.price}</td>
+          </tr>
+        ))}
+      </table>
     </>
   )
 }
